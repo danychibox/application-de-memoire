f237a6ef50d30470d31d5b7af901aee69c404bac
dany <chibululandadany@gmail.com>
2022-09-26 21:40:41 +0200

    cas par structure

---
+++ casparzone.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>minsante</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="stylesheet" type="text/css" href="css/admin.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+</head>
+<body>
+	<header>
+		<div id="hamburger">
+			<div id="hamburger-content">
+				<nav>
+					<ul>
+						<li><a href="index.php">accueil</a></li>
+						<li><a href="#">information</a></li>
+						<li class="monTitre">administration du système</li>
+					</ul>
+				</nav>
+			</div>
+			<button id="hamburger-button">&#9776;</button>
+			<div id="hamburger-sidebar">
+				<div id="hamburger-sidebar-head"></div>
+				<div id="hamburger-sidebar-body"></div>
+			</div>
+			<div id="hamburger-overlay"></div>
+		</div>
+	</header>
+	<div class="row">
+		<div class="col-2 menu">
+		<a href="index.html"><button class="monBouton">accueil</button></a>
+			<a href="controle-utilisateur.php"><button class="monBouton">utilisateurs</button></a>
+			<a href="zonesante.php"><button class="monBouton">zones de santé</button></a>
+			<a href="structure.php"><button class="monBouton">structures sanitaires</button></a>
+			<a href=""><button class="monBouton">maladie</button></a>
+			<a href="nouveau.php"><button class="monBouton">ajout cas</button></a>
+			<a href="liste.php"><button class="monBouton">listes des maladies</button></a>
+			<a href="liststructure.php"><button class="monBouton">liste des structures</button></a>
+			<a href=""><button class="monBouton">graphique des cas</button></a>
+			<a href=""><button class="monBouton">cas par zone</button></a>
+			<a href=""><button class="monBouton">cas par structure</button></a>
+			<a href=""><button class="monBouton">graphique par maladie</button></a>
+		</div>
+
+		<div class="col-8 contenu">
+		<center><h2>cas par zone de santé</h2></center>
+		<form method="POST" action="casparzone.php" name="valider">
+			<label>selectionner une zone de santé</label>
+			<select class="form-control" name="zone">
+						<?php
+						$connexion=mysqli_connect("localhost","chibox","chibox","bd_memoire");
+						$req="select * from zonedesante";
+						$exec=mysqli_query(
+							$connexion,$req);
+						while($row=mysqli_fetch_array($exec)) {
+							echo "<option value='".$row["id_zone"]."'>".$row["designation"]."</option>";
+						}
+						?>
+					</select>
+			<input type="submit" name="valider" class="btn-primary">
+		</form>
+		<table class="table table-bordered table-striped table-condensed">
+			<thead>
+			<th>code</th>
+			<th>désignation </th>
+			<th>maladie</th>
+			<th>cas</th>
+			<th>date</th>
+			</thead>
+		<?php
+		if(isset($_POST['valider'])){
+			$zone=$_POST['zone'];
+			$connexion=mysqli_connect("localhost","chibox","chibox","bd_memoire");
+			$requete="select * from structure,zonedesante,maladie,casmaladie where structure.designationZone=zonedesante.id_zone and casmaladie.structure=structure.id_structure and id_zone='$zone'";
+			$exec=mysqli_query($connexion,$requete);
+			while($row=mysqli_fetch_array($exec)){
+				echo "<tr><td>".$row['id_structure']."</td><td>".$row['designationStructure']."</td><td>".$row['designation']."</td><td>".$row['nombreDesCas']."</td><td>".$row['xdate']."</td></tr>";
+			}
+		}
+		?>
+		</table>
+		</div>
+	</div>
+	<script type="text/javascript" src="js/view.js"></script>
+</body>
+</html>
