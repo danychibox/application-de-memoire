3ae1bfebee3cd1237fdb87ac150583176a931bcb
dany <chibululandadany@gmail.com>
2022-09-26 21:28:46 +0200

    premier depot

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>minsante</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+	<header>
+		<div id="hamburger">
+			<div id="hamburger-content">
+				<nav>
+					<ul>
+						<li><a href="index.html">accueil</a></li>
+						<li><a href="#">information</a></li>
+						<li><a href="connexion.php">connexion</a></li>
+					</ul>
+				</nav>
+			</div>
+			<button id="hamburger-button">&#9776;</button>
+			<div id="hamburger-sidebar">
+				<div id="hamburger-sidebar-head"></div>
+				<div id="hamburger-sidebar-body"></div>
+			</div>
+			<div id="hamburger-overlay"></div>
+		</div>
+	</header>
+	<div class="container">
+		<div class="row">
+			<div class="col-6">
+				<div class="dps"><p>DIVISION PROVINCIALE DE LA SANTE DU NORD KIVU</p></div>
+			</div>
+			<div class="col-5">
+				<img src="imgs/image1.jpg">
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript" src="js/view.js"></script>
+</body>
+</html>
